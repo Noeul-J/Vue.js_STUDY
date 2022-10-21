b59f904cb981257791ed36936cce244bc2698990
Noeul-J <ggumihj@naver.com>
2022-10-21 13:12:45 +0900

    KIST

---
+++ backend/app.js
@@ -0,0 +1,70 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var dataRouter = require('./routes/data');
+
+var app = express();
+
+//추가한 부분
+var mysql = require('mysql');
+const router = require('./routes/index');
+// Connection 객체 생성 
+var connection = mysql.createConnection({
+    //!!!! KIST 정보에 맞게 바꾸기  
+     host: '161.122.37.174',
+     port: '13307',
+     user: 'kist',
+     password: 'kist',
+     database: 'kist'
+  
+  // host: 'localhost',
+  // port: 3306,
+  // user: 'root',   
+  // password: 'kist',
+  // database: 'kist'  
+});  
+// Connect
+connection.connect(function (err) {   
+  console.error("##################")
+  if (err) {     
+    console.error('mysql connection error');     
+    console.error(err);     
+    throw err;   
+  } 
+});
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/api/data', dataRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ backend/routes/data.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+
+//추가한 부분
+var mysql = require('mysql');
+// Connection 객체 생성 
+var connection = mysql.createConnection({
+ // !!!!KIST 정보에 맞게 바꾸기  
+     host: '161.122.37.174',
+     port: '13307',
+     user: 'kist',
+     password: 'kist',
+     database: 'kist'
+  
+  // host: 'localhost',
+  // port: 3306,
+  // user: 'root',   
+  // password: 'kist',
+  // database: 'kist'  
+});  
+// Connect
+connection.connect(function (err) {   
+  if (err) {     
+    console.error('mysql connection error');     
+    console.error(err);     
+    throw err;   
+  } 
+});
+
+// ============= api =================
+
+// api/data/processList (프로세스 리스트 가져오기)
+router.get('/processList', function (req, res) {
+  connection.query('SELECT * FROM process_name', function (err, rows) {
+    if (err) throw err;
+    res.send(rows);
+  });
+})
+
+// api/data/searchList (조회결과 리스트 가져오기)
+router.get('/searchList', function (req, res) {
+  console.log("searchList");
+  console.log(req.query)
+  
+  if (req.query.selectedProcess == '') {
+    console.log("전체 조회!");
+
+    connection.query("SELECT a.PROCESS_NM, b.* " +
+    "FROM process_name a LEFT JOIN process_result b ON a.PROCESS_ID = b.PROCESS_ID " +
+    "where DATE_FORMAT(b.rundate,'%Y-%m-%d') BETWEEN '" + req.query.startDt + "' " +
+    "and '" + req.query.endDt + "' ", 
+    function (err, rows) {
+      if (err) throw err;
+      res.send(rows);
+    });
+
+
+  } else if (req.query.selectedProcess != '') {
+    console.log("selectedProcess 값이 있는 조회!");
+
+    var sql = "SELECT a.PROCESS_NM, b.* " +
+    "FROM process_name a LEFT JOIN process_result b ON a.PROCESS_ID = b.PROCESS_ID " +
+		"where DATE_FORMAT(b.rundate,'%Y-%m-%d') BETWEEN '" + req.query.startDt + "' " +
+		"and '" + req.query.endDt + "' "+
+		"and b.PROCESS_ID = '"+req.query.selectedProcess+"' " +
+		"and a.PROCESS_ID = '"+req.query.selectedProcess+"'"
+    connection.query(sql, function (err, rows) {
+      if (err) throw err;
+      res.send(rows);
+    });
+  }
+  
+})
+
+// ===================================
+
+
+
+module.exports = router;
